a8764155166a6e605cb429bad95b85afa9a757e7
Trung Nguyen <nghetrung@gmail.com>
2018-10-02 14:19:09 +0700

    chapter 5 - Q7,8,9 added

--- chapter5_filled_bar_turtle.py
+++ chapter5_filled_bar_turtle.py
@@ -10,7 +10,7 @@ Created on Mon Oct  1 09:27:23 2018
 
 # 5.12
 import turtle
-xs = [48, 117, 200, 240, 160, 260, 220]
+xs = [-48, 117, 200, 240, 160, 260, 220]
 
 def create_window_and_turtle(wcolor, tcolor, fillcolor, pensz, title):
     # Create the window
@@ -26,19 +26,38 @@ def create_window_and_turtle(wcolor, tcolor, fillcolor, pensz, title):
     return t, wn
 
 def draw_bar(t, height):
-    t.begin_fill()
-    t.left(90)
-    t.forward(height)
-    t.write('  ' + str(height), font=("Arial", 12, "normal"))
-    t.right(90)
-    t.forward(40)
-    t.right(90)
-    t.forward(height)
-    t.left(90)
-    t.end_fill()
-    t.penup()
-    t.forward(10)
-    t.pendown()
+    if height < 0:
+        t.begin_fill()
+        t.left(90)
+        t.forward(height)
+        t.penup()
+        t.forward(-20)
+        t.write('  ' + str(height), font=("Arial", 12, "normal"))
+        t.forward(20)
+        t.pendown()
+        t.right(90)
+        t.forward(40)
+        t.right(90)
+        t.forward(height)
+        t.left(90)
+        t.end_fill()
+        t.penup()
+        t.forward(10)
+        t.pendown()
+    else:
+        t.begin_fill()
+        t.left(90)
+        t.forward(height)
+        t.write('  ' + str(height), font=("Arial", 12, "normal"))
+        t.right(90)
+        t.forward(40)
+        t.right(90)
+        t.forward(height)
+        t.left(90)
+        t.end_fill()
+        t.penup()
+        t.forward(10)
+        t.pendown()
 
 tess, wn = create_window_and_turtle('hotpink', 'green', 'yellow', 3, 'BAR!')
 
@@ -49,5 +68,28 @@ tess.right(180)
 tess.pendown()
 
 for v in xs:
+    if v >= 200:
+        tess.color('green', 'red')
+    elif v >= 100 and v < 200:
+        tess.color('green', 'yellow')
+    else:
+        tess.color('green', 'green')
     draw_bar(tess, v)
 wn.mainloop()
+
+
+# Q8
+
+"""
+    Modify the turtle bar chart program so that the bar for any value of 200 or more is filled
+    with red, values between [100 and 200) are filled with yellow, and bars representing
+    values less than 100 are filled with green.
+"""
+
+# Q9
+
+"""
+    In the turtle bar chart program, what do you expect to happen if one or more of the data
+    values in the list is negative? Try it out. Change the program so that when it prints the
+    text value for the negative bars, it puts the text below the bottom of the bar
+"""
