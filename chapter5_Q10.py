8bb2a87fa115da93d92e8278b5261e51c8f72873
Trung Nguyen <nghetrung@gmail.com>
2018-10-02 14:17:38 +0700

    chapter 5 - Q10 added

---
+++ chapter5_Q10.py
@@ -0,0 +1,7 @@
+# Q10
+""" 
+    Write a function find_hypot which, given the length of two sides of a right-angled
+    triangle, returns the length of the hypotenuse. (Hint: x ** 0.5 will return the square
+    root.) 
+"""
+
