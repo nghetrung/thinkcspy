edcd17c0ef6f2c8da5cf09ac1a829e4026ab45de
Trung Nguyen <nghetrung@gmail.com>
2018-10-03 10:24:48 +0700

    Q1 - updated

--- chapter6.py
+++ chapter6.py
@@ -11,17 +11,21 @@ def turn_clockwise(p):
     elif p == 'W':
         next_point = 'N'
     else:
-        print('Argument is not valid!')
+        next_point = 'None'
     return next_point
 
 class TestStringMethods(unittest.TestCase):
 
     def test_north(self):
-        self.assertEqual(turn_clockwise('N'), 'E')
+        self.assertEqual(turn_clockwise('N'), 'E', 'Next point should be East!')
     def test_east(self):
-        self.assertEqual(turn_clockwise('E'), 'A', 'WRONGGGG')
+        self.assertEqual(turn_clockwise('E'), 'S', 'Next point should be South!')
     def test_south(self):
-        self.assertEqual(turn_clockwise('S'), 'N')
+        self.assertEqual(turn_clockwise('S'), 'W', 'Next point should be West!')
+    def test_west(self):
+        self.assertEqual(turn_clockwise('W'), 'N', 'Next point should be North!')
+    def test_none(self):
+        self.assertEqual(turn_clockwise('haha'), 'None')
 
 if __name__ == '__main__':
     unittest.main()
